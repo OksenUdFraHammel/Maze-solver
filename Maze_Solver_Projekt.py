d663f2d9f96ed36d9c71c4007aabf4fa94911fa1
Oksen ud fra Hammel <andr699g@edu.sde.dk>
2021-02-11 08:15:52 +0100

    The last update to the program with a working user interface

--- Maze_Solver_Projekt.py
+++ Maze_Solver_Projekt.py
@@ -2,6 +2,25 @@ import pygame as pg
 import time
 
 maze1 = [
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1],
+    [1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1 ,0 ,1 ,0 ,0 ,0 ,0 ,0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1],
+    [1, 0, 1 ,0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+
+maze2 = [
     [1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 0, 0, 1, 0, 1],
@@ -13,12 +32,6 @@ maze1 = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
-
-maze2 = [
-    [1,0,1,1],
-    [1,0,1,1],
-    [1,0,0,0],
-    [1,1,1,1]]
 
 start = 0,1
 end = 0,3
@@ -74,73 +87,157 @@ for i in range (len(m)):
 
 #print(m)
 #print(P)
-print(max(min_moves)-1)
+#print(max(min_moves)-1)
 
 pg.init()
+
+def print_moves():
+    moves.append(1)
+    pg.draw.rect(screen,(50,50,50), pg.Rect(805,5,190,145))
+    text = font1.render("Min. {}".format((max(min_moves)-1)), True,(255,255,255))
+    text_rect = text.get_rect()
+    text_rect.center = (900,45)
+    screen.blit(text,text_rect)
+    text = font1.render("Cur. {}".format(len(moves)), True, (255,255,255))
+    text_rect.center = (900,95)
+    screen.blit(text,text_rect)
+    
+
 L = 50 # Pixel height and width of tiles
 
-screen = pg.display.set_mode((len(maze1)*L,len(maze1)*L)) # Window size
+#screen = pg.display.set_mode(((len(maze1)+int(len(maze1)))*L+5,len(maze1)*L+5)) # Window size
+screen = pg.display.set_mode((1000,805))
+font1 = pg.font.Font("Inconsolata-Bold.ttf", 35)
+font2 = pg.font.Font("Inconsolata-Bold.ttf", 28)
+font3 = pg.font.Font("Inconsolata-Bold.ttf", 20)
+
+state = "PLAY"
 
 moves = []
 
-player = []
+player = [start]
 
-player.append((start))
+pg.draw.rect(screen,(50,50,50), pg.Rect(805,5,190,145))
+text = font1.render("Min. {}".format((max(min_moves)-1)), True,(255,255,255))
+text_rect = text.get_rect()
+text_rect.center = (900,45)
+screen.blit(text,text_rect)
+text = font1.render("Cur. {}".format(len(moves)), True, (255,255,255))
+text_rect.center = (900,95)
+screen.blit(text,text_rect)
 
 running = True
 while running:
+    if state == "PLAY":
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                # Close window
+                running = False
+            elif event.type == pg.KEYDOWN:
+                #if event.key == pg.K_ESCAPE:
+                    #state = "PAUSE"
+                if event.key == pg.K_w:
+                    if not maze1[r-1][c] == 1:
+                        head_r,head_c = player[0]
+                        new_head = (head_r-1,head_c)
+                        player.insert(0,new_head)
+                        player.pop()
+                        print_moves()
+                    else:
+                        pass
+                elif event.key == pg.K_s:
+                    if not maze1[r+1][c] == 1:
+                        head_r,head_c = player[0]
+                        new_head = (head_r+1,head_c)
+                        player.insert(0,new_head)
+                        player.pop()
+                        print_moves()
+                    else:
+                        pass
+                elif event.key == pg.K_d:
+                    if not maze1[r][c+1] == 1:
+                        head_r,head_c = player[0]
+                        new_head = (head_r,head_c+1)
+                        player.insert(0,new_head)
+                        player.pop()
+                        print_moves()
+                    else:
+                        pass
+                elif event.key == pg.K_a:
+                    if not maze1[r][c-1] == 1:
+                        head_r,head_c = player[0]
+                        new_head = (head_r,head_c-1)
+                        player.insert(0,new_head)
+                        player.pop()
+                        print_moves()
+                    else:
+                        pass
+                     
+        # Draw game board
+        for c in range(len(maze1)):      # Create variables c and r for width and height
+            for r in range(len(maze1)):
+                pg.draw.rect(screen,(50,50,50), pg.Rect(c*L+5,r*L+5,L-5,L-5))
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            # Close window
-            running = False
-        elif event.type == pg.KEYDOWN:
-            #if event.key == pg.K_ESCAPE:
-                #state = "PAUSE"
-            if event.key == pg.K_w:
-                head_r,head_c = player[0]
-                new_head = (head_r-1,head_c)
-                player.insert(0,new_head)
-                player.pop()
-                moves.append(1)
-                print(len(moves))
-            elif event.key == pg.K_s:
-                head_r,head_c = player[0]
-                new_head = (head_r+1,head_c)
-                player.insert(0,new_head)
-                player.pop()
-                moves.append(1)
-                print(len(moves))
-            elif event.key == pg.K_d:
-                head_r,head_c = player[0]
-                new_head = (head_r,head_c+1)
-                player.insert(0,new_head)
-                player.pop()
-                moves.append(1)
-                print(len(moves))
-            elif event.key == pg.K_a:
-                head_r,head_c = player[0]
-                new_head = (head_r,head_c-1)
-                player.insert(0,new_head)
-                player.pop()
-                moves.append(1)
-                print(len(moves))
+            # Draw walls
+        for c in range(len(maze1)):
+            for r in range(len(maze1[c])):
+                if maze1[r][c] == 1:
+                    pg.draw.rect(screen,(50,200,50), pg.Rect(c*L+5,r*L+5,L-5,L-5))
 
-    # Draw game board
-    for c in range(len(maze1)):      # Create variables c and r for width and height
-        for r in range(len(maze1)):
-            pg.draw.rect(screen,(50,50,50), pg.Rect(c*L+5,r*L+5,L-5,L-5))
+        # Draw player
+        for p in player:
+            r,c = p
+            pg.draw.rect(screen,(200,50,50), pg.Rect(c*L+5,r*L+5,L-5,L-5))
 
-    # Draw walls
-    for c in range(len(maze1)):
-        for r in range(len(maze1[c])):
-            if maze1[r][c] == 1:
-                pg.draw.rect(screen,(50,200,50), pg.Rect(c*L+5,r*L+5,L-5,L-5))
+        if player == [end]:
+            state = "END"
+           
+        # update the screen window
+        pg.display.flip()
 
-    for p in player:
-        r,c = p
-        pg.draw.rect(screen,(200,50,50), pg.Rect(c*L+5,r*L+5,L-5,L-5))
-            
-    # update the screen window
-    pg.display.flip()
+    elif state == "END":
+        pg.draw.rect(screen,(200,200,200), pg.Rect(205,205,395,395))
+        text = font2.render("Congratulations!", True,(0,0,0))
+        text_rect = text.get_rect()
+        text_rect.center = (402,280)
+        screen.blit(text,text_rect)
+        text = font3.render("You completed the maze in {} moves".format(len(moves)), True,(0,0,0))
+        text_rect.center = (342,350)
+        screen.blit(text,text_rect)
+        text = font2.render("Press [SPACE] to try again", True,(0,0,0))
+        text_rect.center = (333,500)
+        screen.blit(text,text_rect)
+        if int(len(moves)) - int(max(min_moves)-1) == 0:
+            text = font3.render("Perfect run!", True,(0,0,0))
+            text_rect.center = (452,400)
+            screen.blit(text,text_rect)   
+        else:
+            text = font3.render("Try to complete it in {} less moves".format(int(len(moves)-int(max(min_moves)-1))), True,(0,0,0))
+            text_rect.center = (340,400)
+            screen.blit(text,text_rect)
+        
 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                # Close window
+                running = False
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    state = "RESET"
+
+        pg.display.flip()
+
+    elif state == "RESET":
+        moves = []
+        player = [start]
+        screen.fill((0,0,0))
+        pg.draw.rect(screen,(50,50,50), pg.Rect(805,5,190,145))
+        text = font1.render("Min. {}".format((max(min_moves)-1)), True,(255,255,255))
+        text_rect = text.get_rect()
+        text_rect.center = (900,45)
+        screen.blit(text,text_rect)
+        text = font1.render("Cur. {}".format(len(moves)), True, (255,255,255))
+        text_rect.center = (900,95)
+        screen.blit(text,text_rect)
+
+        state = "PLAY"
